44f66794572e2ed5803d533f41a543c7d467dad3
Himanshu Gohil <himanshugohil828@gmail.com>
2026-04-02 15:52:09 +0530

    Initial commit - terraform practice

---
+++ ec2.tf
@@ -0,0 +1,80 @@
+#key pair 
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "terraform-key-ec2"
+  public_key = file("terraform-key-ec2.pub")
+}
+
+#vpc and sg
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "automate-sg"
+  description = "this will add a TF genrated security group"
+  vpc_id      = aws_default_vpc.default.id
+
+  #inbound rules 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow ssh from anywhere"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow http from anywhere"
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow my app from anywhere"
+  }
+
+  #outbound rules 
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "all access open outbound"
+  }
+
+  tags = {
+    Name = "automate-sg"
+  }
+
+}
+
+#ec2 instance 
+
+resource "aws_instance" "my_instance" {
+
+  ami = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.my_key.key_name
+
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "him-ec2"
+  }
+
+}
