351df26e01fc0d963de8d28204dc18d221fd1775
Himanshu Gohil <himanshugohil828@gmail.com>
2026-04-07 17:46:08 +0530

    Delete install_nginx.sh

--- install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install nginx -y
-sudo systemctl start nginx 
-sudo systemctl enable nginx
-
-echo "<h1> Welcome to namma Rcb Fan Club </h1>" | sudo tee /var/www/html/index.html
